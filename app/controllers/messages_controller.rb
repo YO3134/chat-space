7f6f41d23802d515484ad2521ac46a7ea5fe4d4c
Y.otaki <nishiura.145@gmail.com>
2018-05-30 08:57:25 +0900

    add_side_bar_function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,7 +25,5 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
-    # id = params[:group_id] || params[:id]
-    # @group = Group.find(id)
   end
 end
