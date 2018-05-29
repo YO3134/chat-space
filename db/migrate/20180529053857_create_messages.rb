9488e2be7ccad0371de58b22f4571b179b531f79
Y.otaki <nishiura.145@gmail.com>
2018-05-29 19:15:11 +0900

    edit

--- db/migrate/20180529053857_create_messages.rb
+++ db/migrate/20180529053857_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.references :user null:false, foreign_key: true
-      t.references :group null:false, foreign_key: true
+      t.references :group, null:false, foreign_key: true
+      t.references :user, null:false, foreign_key: true
       t.string :image
       t.text :body
       t.timestamps
