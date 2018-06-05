e70ffafaaf64144045478b5da1f2ca1c6150c985
Y.otaki <nishiura.145@gmail.com>
2018-06-05 14:45:22 +0900

    add_asynchronous_communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,31 @@
 $(function() {
-  function buildHTML(comment) {
+  function buildHTML(message) {
+    if(message.image.present?){
+    var image = "<img src=${message.image}>"
+    } else {
+      var image = ""
+    }
+
     var html = `<div class="message">
                   <div class="upper_message">
-                    <div class="upper_message__user_name">
-                      <%= message.body %>
-                    </div>
-                    <div class="upper_message__date">
-                      <%= form_posted_time(message.created_at) %>
-                    </div>
+                    <p class="upper_message__user_name">
+                      ${message.user.name}
+                    </p>
+                    <p class="upper_message__date">
+                      ${form_posted_time(messages.created_at)}
+                    </p>
                   </div>
                   <div class="lower_message">
-                    <% if message.body.present? %>
-                      <div class="lower_message__text">
-                        <%= message.body %>
-                      </div>
-                    <% end %>
-                    <%= image_tag message.image, class: 'lower_message__text' if message.image.present? %>
+                      <p class="lower_message__text">
+                        ${image}
+                      <img src=${messages.image.}>
+                        ${message.body}
+                      </p>
                   </div>
                 </div>`
     return html
   }
+
   $(".new_message").on("submit",function(e) {
     e.preventDefault();
     var formData = new FormData(this);
