0b908d2079da8c11922c3a7e190d99f5450509a3
Y.otaki <nishiura.145@gmail.com>
2018-06-07 19:44:42 +0900

    add_html

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,19 +1,17 @@
 $(function() {
 
-var search_list = $(".chat-group-form__field--right");
+var users_list = $(".chat-group-form__input");
 
 function appendUser(user) {
-  var html = `<div class='chat-group-form__field--left'>
-                  <label class="chat-group-form__label" for="chat_group_チャットメンバーを追加">チャットメンバーを追加</label>
-              </div>
-              <div class='chat-group-form__field--right'>
-                  <div class='chat-group-form__search clearfix'>
-                    <input class='chat-group-form__input' id='user-search-field' placeholder='追加したいユーザー名を入力してください' type='text'>
-                  </div>
-                <div id='user-search-result'>
-                </div>
-               </div>`
-  search_list.append(html);
+  var html = `<div class='chat-group-users_list'>
+                < p class= 'chat-group-user__name'>
+                  ${user.name}
+                </p>
+                < a class="add_chat-group-user" data_user_id= "${user.id}" data_user_name ="${user.name}">
+                  <p class="add_chat_group_name">追加</p>
+                </a>
+              </div>`
+  users_list.append(html);
 }
 
 function appendNoUser(user) {
@@ -23,23 +21,22 @@ function appendNoUser(user) {
                   </div>
                   <div id='user-search-result'></div>
                </div>`
-  search_list.append(html);
+  users_list.append(html);
 }
 
   $(".chat-group-form__input").on("keyup", function() {
-    var label = $(".chat-group-form__input").val();
+    var input = $.trim($(this).val());
     console.log(input);
-
     $.ajax({
       type: 'GET',
-      url: '/users/index',
-      data: { keyword: input },
+      url: '/users',
+      data: { name: input },
       dataType: 'json'
     })
 
     .done(function(users) {
-      $(".chat-group-form__field--right").empty();
-      if(users.length !== 0) {
+      // $(".chat-group-form__field--right").();
+      if(users.length !== 0 ) {
         users.forEach(function(user) {
           appendUser(user);
         });
@@ -53,56 +50,6 @@ function appendNoUser(user) {
     });
   });
 });
-
-
-
-    // $.ajax({
-    //   type: 'GET',
-    //   url:  '/users/index'
-    //   data: { keyword:input },
-    //   dataType: 'json'
-    // })
-
-
-  // $(".chat-group-form__field--left").on("keyup", function(e) {
-  //   var label = $(".chat-group-form__label").val();
-
-
-   // console.log(input);
-
-//     .done(function(users) {
-//       $(".hidden").empty();
-//       if (users.length !== 0) {
-//         users.forEach(function(user){
-//           appendUser(user);
-//         });
-//       }
-//       else {
-//         appendNOUser("一致するユーザーはいません");
-//       }
-//     })
-//   });
-// });
-
-
-
-
-
-
-//     .fail(function() {
-//       alert('error');
-//     })
-//     return false;
-//   });
-// });
-
-
-
-
-
-
-
-
 
 //   <div id='chat-group-users'>
 //     <div class='chat-group-user clearfix' id='chat-group-user-22'>
